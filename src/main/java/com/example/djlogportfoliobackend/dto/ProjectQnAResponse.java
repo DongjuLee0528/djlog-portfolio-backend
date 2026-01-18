08756675b23fb2ccaa5876c355e219208b6cf1e0
DongjuLee <dozoo0528@gmail.com>
2026-01-18 14:08:20 +0900

    주석

--- src/main/java/com/example/djlogportfoliobackend/dto/ProjectQnAResponse.java
+++ src/main/java/com/example/djlogportfoliobackend/dto/ProjectQnAResponse.java
@@ -4,11 +4,22 @@ import lombok.Data;
 
 import java.util.UUID;
 
+/**
+ * 프로젝트 Q&A 정보 응답을 위한 DTO
+ * 클라이언트에게 프로젝트 Q&A 정보를 전송하기 위한 데이터 전송 객체
+ */
 @Data
 public class ProjectQnAResponse {
 
+    /** Q&A의 고유 식별자 */
     private UUID id;
+
+    /** Q&A가 속한 프로젝트의 ID */
     private UUID projectId;
+
+    /** 질문 내용 */
     private String question;
+
+    /** 질문에 대한 답변 내용 */
     private String answer;
 }
