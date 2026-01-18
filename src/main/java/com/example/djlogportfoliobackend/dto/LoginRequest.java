566083abeb8018c4c33ad57b688c780110d969ec
DongjuLee <dozoo0528@gmail.com>
2026-01-18 14:08:13 +0900

    주석

--- src/main/java/com/example/djlogportfoliobackend/dto/LoginRequest.java
+++ src/main/java/com/example/djlogportfoliobackend/dto/LoginRequest.java
@@ -4,13 +4,19 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 관리자 로그인 요청을 위한 DTO
+ * 클라이언트로부터 로그인 인증 정보를 받기 위한 데이터 전송 객체
+ */
 @Getter
 @Setter
 public class LoginRequest {
 
+    /** 로그인용 이메일 주소 */
     @NotBlank(message = "Email is required")
     private String email;
 
+    /** 로그인용 비밀번호 */
     @NotBlank(message = "Password is required")
     private String password;
 }
