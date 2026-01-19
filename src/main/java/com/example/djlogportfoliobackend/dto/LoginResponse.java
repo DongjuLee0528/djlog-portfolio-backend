c285f6b596421a7c3fb94ca7c038910e455fd0b2
DongjuLee <dozoo0528@gmail.com>
2026-01-19 21:26:11 +0900

    주석

--- src/main/java/com/example/djlogportfoliobackend/dto/LoginResponse.java
+++ src/main/java/com/example/djlogportfoliobackend/dto/LoginResponse.java
@@ -4,9 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 로그인 응답 DTO 클래스
+ * 사용자 로그인 성공 시 반환되는 정보를 담고 있습니다.
+ */
 @Getter
 @Setter
 @AllArgsConstructor
 public class LoginResponse {
+    /**
+     * JWT 액세스 토큰
+     * 인증된 사용자의 API 요청에 사용됩니다.
+     */
     private String accessToken;
 }
