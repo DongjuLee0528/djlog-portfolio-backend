e1438b4245c3a24a49c7b7cdbbbfc8b89bc9339e
DongjuLee <dozoo0528@gmail.com>
2026-01-26 12:42:04 +0900

    주석

--- src/main/java/com/example/djlogportfoliobackend/config/SecurityHeadersConfig.java
+++ src/main/java/com/example/djlogportfoliobackend/config/SecurityHeadersConfig.java
@@ -10,6 +10,19 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+/**
+ * 보안 헤더 설정 클래스
+ *
+ * 웹 애플리케이션의 보안성을 강화하기 위해 다양한 HTTP 보안 헤더를 자동으로 추가한다.
+ *
+ * 설정되는 보안 헤더:
+ * - X-Content-Type-Options: MIME 스니핑 공격 방지
+ * - X-Frame-Options: 클릭재킹 공격 방지
+ * - X-XSS-Protection: XSS 공격 차단
+ * - Content-Security-Policy: 콘텐츠 보안 정책
+ * - Strict-Transport-Security: HTTPS 강제 사용 (HTTPS 환경에서만)
+ * - Permissions-Policy: 브라우저 API 접근 제한
+ */
 @Configuration
 public class SecurityHeadersConfig {
 
