272295403c05cb06c62a9d4305b6dac6e9233874
DongjuLee <dozoo0528@gmail.com>
2026-02-05 21:52:23 +0900

    주석

--- src/main/java/com/example/djlogportfoliobackend/dto/ProfileResponse.java
+++ src/main/java/com/example/djlogportfoliobackend/dto/ProfileResponse.java
@@ -6,7 +6,23 @@ import java.util.List;
 import java.util.UUID;
 
 /**
- * 프로필 정보를 담는 응답 DTO
+ * 프로필 정보 조회 응답 DTO
+ *
+ * <p>클라이언트에게 프로필 정보를 전송하기 위한 데이터 전송 객체입니다.
+ * 개인 정보, 기술 스택, 연락처 정보 등을 포함하여 포트폴리오 웹사이트의
+ * 다양한 페이지에서 활용됩니다.</p>
+ *
+ * <p>주요 사용 위치:</p>
+ * <ul>
+ *   <li>포트폴리오 메인 페이지 - 프로필 섹션 표시</li>
+ *   <li>About 페이지 - 상세 정보 및 자기소개 표시</li>
+ *   <li>관리자 페이지 - 프로필 편집 화면 데이터 로드</li>
+ *   <li>Contact 섹션 - 연락처 정보 표시</li>
+ * </ul>
+ *
+ * @author DJ
+ * @version 1.0
+ * @since 2024
  */
 @Data
 public class ProfileResponse {
