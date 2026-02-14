0a2e238b88354ae051fa2dbbf2e82a7eb8652ad5
DongjuLee <dozoo0528@gmail.com>
2026-02-14 13:00:46 +0900

    주석

--- src/main/java/com/example/djlogportfoliobackend/dto/CertificateRequest.java
+++ src/main/java/com/example/djlogportfoliobackend/dto/CertificateRequest.java
@@ -5,6 +5,10 @@ import lombok.Data;
 
 import java.time.LocalDate;
 
+/**
+ * 자격증 정보 등록/수정 요청 DTO
+ * 클라이언트로부터 자격증 정보를 받기 위한 데이터 전송 객체
+ */
 @Data
 public class CertificateRequest {
     @NotBlank(message = "자격증명은 필수입니다")
