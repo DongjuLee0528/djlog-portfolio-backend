52c0ffef638e5d6cacaba6c3f2db13070ee16e71
DongjuLee <dozoo0528@gmail.com>
2026-02-14 13:00:52 +0900

    주석

--- src/main/java/com/example/djlogportfoliobackend/dto/CertificateResponse.java
+++ src/main/java/com/example/djlogportfoliobackend/dto/CertificateResponse.java
@@ -6,6 +6,10 @@ import lombok.Data;
 import java.time.LocalDate;
 import java.util.UUID;
 
+/**
+ * 자격증 정보 응답 DTO
+ * 클라이언트에게 자격증 정보를 전달하기 위한 데이터 전송 객체
+ */
 @Data
 public class CertificateResponse {
     private UUID id;
@@ -14,6 +18,10 @@ public class CertificateResponse {
     private LocalDate issueDate;
     private String credentialId;
 
+    /**
+     * Certificate 엔티티로부터 응답 DTO를 생성하는 생성자
+     * @param certificate 변환할 Certificate 엔티티
+     */
     public CertificateResponse(Certificate certificate) {
         this.id = certificate.getId();
         this.name = certificate.getName();
