7ee33d969f3615ed2f6e1e4c10f441d51dd85eb4
DongjuLee <dozoo0528@gmail.com>
2026-02-15 20:02:16 +0900

    주석

--- src/main/java/com/example/djlogportfoliobackend/dto/EducationRequest.java
+++ src/main/java/com/example/djlogportfoliobackend/dto/EducationRequest.java
@@ -3,6 +3,10 @@ package com.example.djlogportfoliobackend.dto;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
+/**
+ * 학력 정보 등록/수정 요청 DTO
+ * 클라이언트로부터 학력 정보를 받기 위한 데이터 전송 객체
+ */
 @Data
 public class EducationRequest {
     @NotBlank(message = "학교명은 필수입니다")
