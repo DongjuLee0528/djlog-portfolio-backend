b79a5c2a58abe924ee9d1060c1ce859741c7866d
DongjuLee <dozoo0528@gmail.com>
2026-02-15 20:02:31 +0900

    주석

--- src/main/java/com/example/djlogportfoliobackend/dto/EducationResponse.java
+++ src/main/java/com/example/djlogportfoliobackend/dto/EducationResponse.java
@@ -5,6 +5,10 @@ import lombok.Data;
 
 import java.util.UUID;
 
+/**
+ * 학력 정보 응답 DTO
+ * 클라이언트에게 학력 정보를 전달하기 위한 데이터 전송 객체
+ */
 @Data
 public class EducationResponse {
     private UUID id;
@@ -13,6 +17,10 @@ public class EducationResponse {
     private String period;
     private String degree;
 
+    /**
+     * Education 엔티티로부터 응답 DTO를 생성하는 생성자
+     * @param education 변환할 Education 엔티티
+     */
     public EducationResponse(Education education) {
         this.id = education.getId();
         this.school = education.getSchool();
